c7fb595bc2d60d8708b4ba3532d9651070a6a8ef
Vincent Labatut <vince.labatut@gmail.com>
2017-01-27 17:20:56 +0100

    updated extraction script

--- src/misc/extraction.R
+++ src/misc/extraction.R
@@ -8,7 +8,7 @@
 # Vincent Labatut 01/2017
 #
 # setwd("~/eclipse/workspaces/Networks/SpatialMeasures")
-# setwd("c:/eclipse/workspaces/Networks/SpatialMeasures")
+# setwd("d:/eclipse/workspaces/Networks/SpatialMeasures")
 # source("src/misc/extraction.R")
 ############################################################################
 library("tools")
@@ -30,15 +30,19 @@ cities <- list(
 #	 alicesprings=c(133.8220, -23.7248, 133.9050, -23.6639),
 #	      avignon=c(  4.7669,  43.9240,   4.8455,  43.9619),
 #	       beijin=c(116.2635,  39.8233, 116.4887,  39.9897),
-# 		 bordeaux=c( -0.6805,  44.7757,  -0.4968,  44.8924),	#TODO
-# 			dakar=c(-17.5338,  14.6434, -17.4185,  14.8104),	#TODO
-# 		 hongkong=c(114.1150,  22.1912, 114.2640,  22.2961),	#TODO
+# 		 bordeaux=c( -0.6805,  44.7757,  -0.4968,  44.8924),
+# 			dakar=c(-17.5338,  14.6434, -17.4185,  14.8104),
+# 		 hongkong=c(114.1150,  22.1912, 114.2640,  22.2961),
 #	     istanbul=c( 28.9208,  40.9846,  29.0671,  41.0721),
 #	   karlskrona=c( 15.5604,  56.1488,  15.6016,  56.1704),
-# 		   lisbon=c( -9.1810,  38.6973,  -9.0938,  38.7511),	#TODO
-# 		ljubljana=c( 14.4455,  46.0116,  14.5790,  46.0857),	#TODO
-# 		manhattan=c(-74.0224,  40.6994, -73.9219,  40.8510),	#TODO
+# 		   lisbon=c( -9.1810,  38.6973,  -9.0938,  38.7511),
+#		liverpool=c( -3.0006,  53.3875,  -2.8952,  53.4489),
+# 		ljubljana=c( 14.4455,  46.0116,  14.5790,  46.0857),
+ 	   maastricht=c(  5.6767,  50.8394,   5.7340,  50.8616),
+# 		manhattan=c(-74.0224,  40.6994, -73.9219,  40.8510),
 ##	      newyork=c(-74.0465,  40.5389, -73.7787,  40.9083),
+	 stpetersburg=c( 30.1671,  59.8690,  30.4823,  60.0189)
+#		     roma=c( 12.4212,  41.8696,  12.5356,  41.9434),
 #	         sfax=c( 10.6272,  34.6507,  10.8614,  34.8640),
 #	     soustons=c( -1.4313,  43.7396,  -1.3158,  43.7793),
 #		    tokyo=c(139.5895,  35.5378, 139.9136,  35.8590),
@@ -49,44 +53,44 @@ cities <- list(
 
 
 # process each city
-for(c in 1:length(cities))
-{	name <- names(cities)[c]
-	cat("Processing city ",name,"\n",sep="")
-	city.folder <- file.path(urban.folder,name)
-	
-	osm.file <- file.path(city.folder,"data.osm")
-#	src <- osmsource_api()
-	src <- osmsource_file(osm.file)
-
-	box <- corner_bbox(cities[[name]][1], cities[[name]][2], cities[[name]][3], cities[[name]][4])	#large
-	city <- get_osm(box, source=src)
-	
-	# extract the graph of streets
-	street.ids <- find(city, way(tags(k=="highway")))		# get the ids of the highways
-	#city.sub <- subset(city, way_ids=street.ids)			# get the subset of the data containing these streets
-	#street.ids <- find(city.sub, way(tags(k=="name")))		# in this subset, find the named streets
-	nal.ids <- find_down(city, way(street.ids))				# get the ids of the ways and nodes constituting the named streets
-	city.sub <- subset(city, ids=nal.ids)					# get the subset of the data containing these streets and nodes
-	
-	# convert to an igraph object (with spatial coordinates)
-	g <- as_igraph(city.sub)
-	g <- as.undirected(graph=g,mode="collapse")
-	idx <- match(V(g)$name,city.sub$nodes$attrs[,"id"])
-	V(g)$x <- city.sub$nodes$attrs[idx,"lon"]*1000			# multiply by 1000 so that the network can be browsed in Gephi
-	V(g)$y <- city.sub$nodes$attrs[idx,"lat"]*1000
-	
-	# filter the nodes located out of the box
-	idx <- which(V(g)$x<cities[[name]][1] || V(g)$y<cities[[name]][3] || V(g)$x>cities[[name]][3] || V(g)$y>cities[[name]][4])
-	print(length(idx))
-	g <- delete_vertices(graph=g, v=idx)
-
-	plot(g,vertex.label=NA,vertex.size=1)
-	
-	# export as graphml
-	#net.file <- paste0(file_path_sans_ext(osm.file),".graphml")
-	net.file <- file.path(city.folder,"graph.graphml")
-	write.graph(g, net.file, format="graphml")
-}
+#for(c in 1:length(cities))
+#{	name <- names(cities)[c]
+#	cat("Processing city ",name,"\n",sep="")
+#	city.folder <- file.path(urban.folder,name)
+#	
+#	osm.file <- file.path(city.folder,"data.osm")
+##	src <- osmsource_api()
+#	src <- osmsource_file(osm.file)
+#
+#	box <- corner_bbox(cities[[name]][1], cities[[name]][2], cities[[name]][3], cities[[name]][4])	#large
+#	city <- get_osm(box, source=src)
+#	
+#	# extract the graph of streets
+#	street.ids <- find(city, way(tags(k=="highway")))		# get the ids of the highways
+#	#city.sub <- subset(city, way_ids=street.ids)			# get the subset of the data containing these streets
+#	#street.ids <- find(city.sub, way(tags(k=="name")))		# in this subset, find the named streets
+#	nal.ids <- find_down(city, way(street.ids))				# get the ids of the ways and nodes constituting the named streets
+#	city.sub <- subset(city, ids=nal.ids)					# get the subset of the data containing these streets and nodes
+#	
+#	# convert to an igraph object (with spatial coordinates)
+#	g <- as_igraph(city.sub)
+#	g <- as.undirected(graph=g,mode="collapse")
+#	idx <- match(V(g)$name,city.sub$nodes$attrs[,"id"])
+#	V(g)$x <- city.sub$nodes$attrs[idx,"lon"]*1000			# multiply by 1000 so that the network can be browsed in Gephi
+#	V(g)$y <- city.sub$nodes$attrs[idx,"lat"]*1000
+#	
+#	# filter the nodes located out of the box
+#	idx <- which(V(g)$x<cities[[name]][1] || V(g)$y<cities[[name]][3] || V(g)$x>cities[[name]][3] || V(g)$y>cities[[name]][4])
+#	print(length(idx))
+#	g <- delete_vertices(graph=g, v=idx)
+#
+#	plot(g,vertex.label=NA,vertex.size=1)
+#	
+#	# export as graphml
+#	#net.file <- paste0(file_path_sans_ext(osm.file),".graphml")
+#	net.file <- file.path(city.folder,"graph.graphml")
+#	write.graph(g, net.file, format="graphml")
+#}
 
 
 
